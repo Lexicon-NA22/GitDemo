5fbea1d7dde8fe4b395a7ea30dbd6c5b84b69a94
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-11-30 10:23:10 +0100

    Add CW

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -7,6 +7,9 @@ namespace GitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
